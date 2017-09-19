be180bd50d6562590039ceb622099a8248a10561
CelesteCC <418026326@qq.com>
2017-09-19 18:18:52 +0800

    20170919更新

--- src/components/custome.vue
+++ src/components/custome.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="main">
     <div class="content">
-      <audio src=""></audio>
-      <div class="">
+      <audio src="https://m7.music.126.net/20170919180710/b41062302bdccaaf5c201bddad802844/ymusic/1606/426f/10a6/a01cace34f2df73c384bbcfe3e30b827.mp3" autoplay controls="controls"></audio>
+      <div class="audio_top">
         <div class=""></div>
         <div class=""></div>
       </div>
+      <div class="audio_pic"></div>
+      <div class="audio_play"></div>
     </div>
   </div>
 </template>
@@ -14,87 +16,15 @@
     name: 'app',
     data () {
       return {
-        
+
       }
     }
   }
 </script>
 
 <style lang="scss">
-  .con{height: 100%}
-  .aplayer{
-    position: fixed;
-    bottom: 0;
-    z-index: 2;
-    background: #fff;
-    width: 100%;
-    left: 0;
-  }
-  .main{font-size: 0.24rem;height: 100%}
-  .album_pic{
-    display: block;
-    width: 100%;
-  }
-  .songs_content{
-    padding:0 0.2rem;
-    background:#fff;
-    border-radius:0.12rem;
-    height:100%;
-    overflow-y:auto;
-  li{
-    position: relative;
-    height: 1.2rem;
-    display: flex;
-    justify-content: center;
-    align-items:center;
-  &:last-child{
-  :after{
-    content: normal;
-  }
-  }
-  :after{
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 1px;
-    -webkit-transform: scaleY(.5);
-    transform: scaleY(.5);
-    background: #ccc;
-    left: 0;
-    bottom: 0;
-  }
-  .songs_content_info{
-    display: flex;
-    flex: 1;
+  .content{
     height: 100%;
-    justify-content: center;
-    align-items:center;
-  p{
-    flex: 6;
-    text-align: left;
-  span{
-    font-size: 0.24rem;
-    color: #999
   }
-  .songs_content_info_name{
-    display: block;
-    font-size: 0.28rem;
-    color: #333
-  }
-  }
-  .songs_content_info_num{
-    flex: 1;
-    font-size: 0.32rem;
-    color: #999;
-  }
-  }
-  .songs_content_down{
-    width: 1.2rem;
-    height: 100%;
-    background: url("../assets/download.png") center no-repeat;
-    background-size: 0.48rem auto;
-  }
-  }
-  }
-  .songs_content li:last-child:after{content: normal}
+  .audio_top{}
 </style>
